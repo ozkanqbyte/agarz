74fdb4e70b627b825a50451d9169176a7064ba62
ozkanqbyte <ozkanqbyte@gmail.com>
2026-05-04 10:32:45 +0300

    cleanup

--- fix.js
+++
@@ -1,4 +0,0 @@
-const fs=require('fs'); 
-let c=fs.readFileSync('server/server.js','utf8'); 
-c=c.replace(/app\.get\('\/health', \(_, res\) => res\.json\(\{ ok: true \}\)\)\n\n/,''); 
-fs.writeFileSync('server/server.js',c);
--- fix.mjs
+++
@@ -1,6 +0,0 @@
-import fs from 'fs'; 
-let c=fs.readFileSync('server/server.js','utf8'); 
-const before=c.length; 
-c=c.split(\"app.get^('/health', ^(_, res^) =^> res.json^({ ok: true }^)^)\n\n\").join(''); 
-console.log('diff:',before-c.length); 
-fs.writeFileSync('server/server.js',c);
